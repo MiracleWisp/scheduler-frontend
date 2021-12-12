3aa107afb15bb350a09dca75ee0684ae8226468a
Semen Uchvatov <s-uchvatov@yandex.ru>
2021-12-12 20:42:37 +0300

    Extend user model

--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -1,6 +1,12 @@
+import {Role} from "./const/role.enum";
+
 export class User {
   id: string;
   email: string;
   firstName: string;
   lastName: string;
+  isSpecialist: boolean;
+  jobTitle: string;
+  about: string;
+  roles: Role[];
 }
